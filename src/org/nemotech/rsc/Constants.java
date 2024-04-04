e90e48cf103ac689176f38828b3000bc9946f623
kenyy28 <104810014+kenyy28@users.noreply.github.com>
2024-04-03 22:37:20 -0400

    Add files via upload

--- src/org/nemotech/rsc/Constants.java
+++ src/org/nemotech/rsc/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     
     public static final String VERSION = "2.4.2";
     
-    public static int EXPERIENCE_MULTIPLIER = 1 ;
+    public static int EXPERIENCE_MULTIPLIER = 20 ;
 
     public static final boolean MEMBER_WORLD = true;
     
@@ -18,7 +18,7 @@ public class Constants {
 
     public static final String MEMBERS_ONLY_MESSAGE = "This feature is only available on a members server";
     
-    public static final String APPLICATION_TITLE = "RSC Single Player " + VERSION;
+    public static final String APPLICATION_TITLE = "Single RSC " + VERSION;
 
     public static final int APPLICATION_WIDTH = 950, APPLICATION_HEIGHT = 500;
     
