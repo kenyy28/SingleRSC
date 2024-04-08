ee6e580c7ab36a6114ac8dcef69aaa431448f4be
kenyy28 <104810014+kenyy28@users.noreply.github.com>
2024-04-08 18:24:15 -0400

    Add files via upload

--- src/org/nemotech/rsc/Constants.java
+++ src/org/nemotech/rsc/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     
     public static final String VERSION = "2.4.2";
     
-    public static int EXPERIENCE_MULTIPLIER = 20 ;
+    public static int EXPERIENCE_MULTIPLIER = 1 ;
 
     public static final boolean MEMBER_WORLD = true;
     
